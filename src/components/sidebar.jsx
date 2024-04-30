a11484ff8d6f38fd7cab0ab6644d4ca00d9d7248
Alan Lima <alanreislm@gmail.com>
2024-04-30 13:21:12 -0300

    feat: add sidebar

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,5 +1,7 @@
 import styles from './sidebar.module.css';
 
+import { PencilLine } from 'lucide-react';
+
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
@@ -8,11 +10,15 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1614624532983-4ce03382d63d?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
       />
       <div className={styles.profile}>
+        <img src="https://github.com/alanlimadev.png" />
         <strong>Alan Lima</strong>
         <span>Desenvolvedor Web</span>
       </div>
       <footer>
-        <a href="#">Editar seu perfil</a>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   );
