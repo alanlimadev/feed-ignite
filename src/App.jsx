cb258f984f6425afddc3cb3a7613af19ccdcca27
Alan Lima <alanreislm@gmail.com>
2024-04-30 15:04:31 -0300

    feat: add components in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import { Header } from './components/header';
+import { Sidebar } from './components/sidebar';
+import { Post } from './components/post';
+
+import styles from './App.module.css';
 import './global.css';
 
 export function App() {
   return (
     <>
       <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author="John Doe" content="Hello, world!" />
+          <Post author="Jane Doe" content="Goodbye, world!" />
+        </main>
+      </div>
     </>
   );
 }
