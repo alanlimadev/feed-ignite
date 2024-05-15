ea63384bdd73ac74f4ad98a44553f95cbab19044
Alan Lima <alanreislm@gmail.com>
2024-05-15 11:51:45 -0300

    feat: iterating in JSX

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,51 @@ import { Post } from './components/post';
 import styles from './App.module.css';
 import './global.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/alanlimadev.png',
+      name: 'Alan Lima',
+      role: 'Frontend Developer',
+    },
+    content: [
+      { type: 'paragraph', value: 'Fala galeraa 👋' },
+      {
+        type: 'paragraph',
+        value:
+          'Acabei de subir mais um projeto no meu portifa. É um jeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é torCare 🚀',
+      },
+      { type: 'link', value: '👉 jane.design/doctorcare' },
+      { type: 'link', value: '#novoprojeto' },
+      { type: 'link', value: '#nlw' },
+      { type: 'link', value: '#rocketseat' },
+    ],
+    publishedAt: new Date('2024-05-15 12:30:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/pauloherbt.png',
+      name: 'Paulo Herbert',
+      role: 'Backend Developer',
+    },
+    content: [
+      { type: 'paragraph', value: 'Fala galeraa 👋' },
+      {
+        type: 'paragraph',
+        value:
+          'Acabei de subir mais um projeto no meu portifa. É um jeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é torCare 🚀',
+      },
+      { type: 'link', value: '👉 jane.design/doctorcare' },
+      { type: 'link', value: '#novoprojeto' },
+      { type: 'link', value: '#nlw' },
+      { type: 'link', value: '#rocketseat' },
+    ],
+    publishedAt: new Date('2024-05-12 12:30:00'),
+  },
+];
+
 export function App() {
   return (
     <>
@@ -13,8 +58,16 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post author="John Doe" content="Hello, world!" />
-          <Post author="Jane Doe" content="Goodbye, world!" />
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </>
